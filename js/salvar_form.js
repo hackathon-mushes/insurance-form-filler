719b1378f0ddac1ea4fd6d3bf4086cfde4109662
ricardocanela <ricardo_canela123@hotmail.com>
2019-02-16 15:17:27 -0200

    fixing field format

--- js/salvar_form.js
+++ js/salvar_form.js
@@ -3,14 +3,27 @@ window.onload = function(){
 
   botao_salvar.onclick = function(element) {
     var dados = document.getElementById("myForm").elements;
-    // alert(dados[0].name);
 
     //Criação do dicionário com o valor dos elementos
     for (var i = 0; i < dados.length; i++) {
       var nome = dados[i].name
       var valor = dados[i].value
-      chrome.storage.sync.set({[nome]:valor}); //Salva elementos
-      // alert(dados[i].name);
+      if (dados[i].type == "radio") {
+        if (dados[i].checked) {
+          chrome.storage.sync.set({[nome]:valor}); //Salva elementos
+        } else {
+          // dont save
+        }
+      } else {
+        if (dados[i].type == "checkbox") {
+          if(dados[i].checked) {
+            valor = true
+          } else {
+            valor = false
+          }
+        }
+        chrome.storage.sync.set({[nome]:valor}); //Salva elementos
+      }
     };
   };
 };
